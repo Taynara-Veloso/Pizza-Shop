91ae5419a8cf4843f4db51b46ab514c53a325763
taynara veloso <taynarasantos2.3@hotmail.com>
2024-10-09 19:53:13 -0300

    :star: Folder Dashboard

--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AuthLayout } from '@/pages/_layouts/auth'
 import { DefaultLayout } from '@/pages/_layouts/default'
-import { Dashboard } from '@/pages/app/dashboard'
+import { Dashboard } from '@/pages/app/dashboard/dashboard'
 import { Orders } from '@/pages/app/orders/orders'
 import { SignIn } from '@/pages/auth/sign-in'
 import { SignUp } from '@/pages/auth/sign-up'
