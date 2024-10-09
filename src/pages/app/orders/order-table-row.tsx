9ee7797524f53c0884a94d47f749dc86e03895af
taynara veloso <taynarasantos2.3@hotmail.com>
2024-10-09 19:56:11 -0300

    :star: Order Table Content

---
+++ src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,49 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { Dialog, DialogTrigger } from '@/components/ui/dialog'
+import { TableCell, TableRow } from '@/components/ui/table'
+
+import { OrderDetailsDialog } from './order-details-dialog'
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+          <OrderDetailsDialog />
+        </Dialog>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">
+        458d789f8dfghv845h
+      </TableCell>
+      <TableCell className="text-muted-foreground">Há 15 minutos</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">Taynara Santos Veloso</TableCell>
+      <TableCell className="font-medium">R$ 169,99</TableCell>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="outline" size="xs" className="border-red-600">
+          <X className="mr-2 h-3 w-3 text-red-600" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
